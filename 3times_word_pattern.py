3cdee008db124ec55365dd87fe8fd59da6198ee8
Sudha Gorka <100294985+SudhaGorka@users.noreply.github.com>
2024-01-07 23:34:27 +0530

    Add files via upload

---
+++ 3times_word_pattern.py
@@ -0,0 +1,8 @@
+#Write question words 3 times using nested loops
+
+serial_number = [1,2,3]
+weekday = ['Hello' , 'Sudha', 'Beautiful']
+for i in serial_number:
+    print(i)
+for j in weekday:
+    print(j)
